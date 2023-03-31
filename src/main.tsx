45f2a20bcbee751958dd596e95e45c2d30392264
kinxori <116584982+kinxori@users.noreply.github.com>
2023-03-31 03:24:34 -0600

    react router dom config updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouterProps, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from "../routes/App/App"
 import './index.css'
 import Button1 from "../routes/Button1"
@@ -10,49 +10,55 @@ import React from 'react'
 import {loader as AppLoader} from "../routes/App/App"
 
 
-
-const router = createBrowserRouter([
+const routerConfig = [
   {
     path:"/",
     element: <App/>,
-    loader:AppLoader,
+    loader: AppLoader,
     children:[
       {
-      path: "episode1",
-      element: <Button1/>,
-      loader:AppLoader,
-      children: [
-        {
-          path: "details",
-          element: <Details/>,
-        }
-      ]
-    }, 
-    {
-      path: "episode2",
-      element: <Button2/>,
-      loader:AppLoader,
-      children: [
-        {
-          path: "details",
-          element: <Details/>,
-        }
-      ]
-    },
-    {
-      path: "episode3",
-      element: <Button3/>,
-      loader:AppLoader,
-      children: [
-        {
-          path: "details",
-          element: <Details/>,
-        }
-      ]
-    }
-  ]
-}, 
-])
+        path: "episode1",
+        element: <Button1/>,
+        loader: AppLoader,
+
+        children: [
+          {
+            path: "details",
+            element: <Details/>,
+          }
+        ]
+      }, 
+      {
+        path: "episode2",
+        element: <Button2/>,
+        loader: AppLoader,
+
+        children: [
+          {
+            path: "details",
+            element: <Details/>,
+          }
+        ]
+      },
+      {
+        path: "episode3",
+        element: <Button3/>,
+        loader: AppLoader,
+
+        children: [
+          {
+            path: "details",
+            element: <Details/>,
+          }
+        ]
+      }
+    ]
+  }
+]
+
+const browserRouterProps: BrowserRouterProps = { basename: '/' }
+
+const router = createBrowserRouter(routerConfig, browserRouterProps)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
